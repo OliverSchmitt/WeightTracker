9e6a6f9f98f3f04a55d13143353de5ee555a7067
Oliver Schmitt <oliverschmitt@gmx.de>
2019-06-06 19:06:14 +0200

    Style enter page, remove seconds from time display

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,8 +1,7 @@
 function getCurrentDateTime() {
     var date = new Date();
     return ("0" + (date.getUTCHours()+2)).slice(-2) + ":" +
-        ("0" + date.getUTCMinutes()).slice(-2) + ":" +
-        ("0" + date.getUTCSeconds()).slice(-2) + " " +
+        ("0" + date.getUTCMinutes()).slice(-2) + " " +
         ("0" + date.getUTCDate()).slice(-2) + "." +
         ("0" + (date.getUTCMonth()+1)).slice(-2) + " " +
         date.getUTCFullYear();
