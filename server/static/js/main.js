9860d4f88d51b6c59cb89f4636d72d029cba37fc
Oliver Schmitt <oliverschmitt@gmx.de>
2019-06-06 18:36:59 +0200

    Move current time computation to main.js

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,1 +1,9 @@
-// Get current time
+function getCurrentDateTime() {
+    var date = new Date();
+    return ("0" + (date.getUTCHours()+2)).slice(-2) + ":" +
+        ("0" + date.getUTCMinutes()).slice(-2) + ":" +
+        ("0" + date.getUTCSeconds()).slice(-2) + " " +
+        ("0" + date.getUTCDate()).slice(-2) + "." +
+        ("0" + (date.getUTCMonth()+1)).slice(-2) + " " +
+        date.getUTCFullYear();
+}
