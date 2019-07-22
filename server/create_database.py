0a764f89cdf9ca2221ec1e991741177db632bcdc
Oliver Schmitt <oliverschmitt@gmx.de>
2019-07-22 23:35:07 +0200

    Apply non cyclic import

--- server/create_database.py
+++ server/create_database.py
@@ -1,9 +1,9 @@
 import sqlite3
-from server.server import server
-from server.database import get_db
+
+import server
 
 
-ADD_EXAMPLE_WEIGHT_VALUES_QUERY = """INSERT INTO """ + TABLE_NAME + """
+ADD_EXAMPLE_WEIGHT_VALUES_QUERY = """INSERT INTO """ + server.TABLE_NAME + """
     (hour, minute, second, day, month, year, weight)
     VALUES
     (22, 37, 16, 25, 5, 2019, 73.4),
@@ -12,20 +12,20 @@ ADD_EXAMPLE_WEIGHT_VALUES_QUERY = """INSERT INTO """ + TABLE_NAME + """
     (22, 45, 37, 15, 6, 2019, 74.1)
     """
 
-SELECT_QUERY = "SELECT * FROM " + TABLE_NAME
+SELECT_QUERY = "SELECT * FROM " + server.TABLE_NAME
 
 
 def create_database():
     try:
-        return sqlite3.connect(DATABASE)
+        return sqlite3.connect(server.DATABASE)
     except Exception as e:
         print(e)
 
 def get_cursor():
-    return sqlite3.connect(DATABASE).cursor()
+    return sqlite3.connect(server.DATABASE).cursor()
 
 def create_weight_values_table(con):
-    con.cursor().execute(CREATE_TABLE_QUERY)
+    con.cursor().execute(server.CREATE_TABLE_QUERY)
 
 def add_example_weight_values(con):
     con.cursor().execute(ADD_EXAMPLE_WEIGHT_VALUES_QUERY)
@@ -39,9 +39,9 @@ def print_all_weight_values(con):
 
 
 if __name__ == "__main__":
-    with server.app_context():
+    with server.flask_server.app_context():
         create_database()
-        # try:
-        #     get_db().cursor().execute(CREATE_TABLE_QUERY)
-        # except Exception as e :
-        #     print(e)
+        try:
+            server.get_db().cursor().execute(server.CREATE_TABLE_QUERY)
+        except Exception as e :
+            print(e)
